6ec132bd943f00abaab7ec588b3eaea828070ec5
LucasAlesterio <lucasalesteriomarquesvieira@gmail.com>
2021-02-07 02:05:03 -0300

    Remove import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { Container } from '../styles/pokedex';
 import { ItemLink, LinkConteiner, Title, ContainerImage } from '../styles/homePage';
-//import Image from 'next/image';
+
 export default function Home() {
     return (
     <Container>
